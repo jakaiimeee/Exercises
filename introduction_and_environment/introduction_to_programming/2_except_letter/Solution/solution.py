55feae882a542bbb5d566c717fab6d4e63246198
jakaiimeee <jakaiimee12@gmail.com>
2020-05-23 15:18:17 -0400

    solved vowels

--- introduction_and_environment/introduction_to_programming/2_except_letter/Solution/solution.py
+++ introduction_and_environment/introduction_to_programming/2_except_letter/Solution/solution.py
@@ -1,1 +1,10 @@
-
+def disemvowel(word):
+    List = list(word)
+    Vowels = ["a", "e", "i", "o", "u"]
+    for letter in List:
+        if letter.lower() in Vowels:
+            List.remove(letter)
+    word = ''.join(List)
+    return word
+print(disemvowel('Byte Academy'))
+      
