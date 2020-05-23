5118718af1f79893c4971b94931fecd283840f00
jakaiimeee <jakaiimee12@gmail.com>
2020-05-23 18:56:42 -0400

    solved grade

--- introduction_and_environment/data_types_and_control_flow/3_grade/Solution/solution.py
+++ introduction_and_environment/data_types_and_control_flow/3_grade/Solution/solution.py
@@ -1,1 +1,12 @@
+percentage = int(input())
 
+if(percentage >= 90):
+    print("A+ Grade")
+elif(percentage >= 80):
+    print("B Grade")
+elif(percentage >= 70):
+    print("C Grade")
+elif(percentage >= 50):
+    print("D Grade")
+else:
+    print("Fail")
