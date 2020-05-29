ad5e1624d11ce14d786f277616f5b575c987f26f
jakaiimeee <jakaiimee12@gmail.com>
2020-05-29 11:57:11 -0400

    solved sum

--- introduction_to_python/functions/3_total/Solution/solution.py
+++ introduction_to_python/functions/3_total/Solution/solution.py
@@ -1,10 +1,9 @@
-# Code your solution here
 def summ(l):
     total = 0
     for x in l:
         total=total+x 
     return total
 
-l=[10,20,30,40,50,60]
+l=[10,20,30,40,50,65]
 result=summ(l)
 print(result)
