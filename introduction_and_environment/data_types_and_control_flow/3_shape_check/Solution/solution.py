9835938c97b094174aa39bc571527c7c8804e22c
jakaiimeee <jakaiimee12@gmail.com>
2020-05-29 10:46:38 -0400

    not quite

--- introduction_and_environment/data_types_and_control_flow/3_shape_check/Solution/solution.py
+++ introduction_and_environment/data_types_and_control_flow/3_shape_check/Solution/solution.py
@@ -1,1 +1,15 @@
+figure = {3: 'triangle' , 4: 'square', 5: 'pentagon', 6: 'hexagon',
+7: 'heptagon' , 8 : 'Octagon', 9: "nonagon"}
+x = int(input("enter a shapes total sides"))
 
+for key,vaule in figure.items():
+    print(vaule)
+    break
+    
+
+#figure = {3: 'triangle' , 4: 'square', 5: 'pentagon', 6: 'hexagon',
+#7: 'heptagon' , 8 : 'Octagon', 9: "nonagon"}
+#x = int(input("enter a shapes total sides"))
+
+#for key,vaule in figure.items():
+ #   print(vaule[x])
