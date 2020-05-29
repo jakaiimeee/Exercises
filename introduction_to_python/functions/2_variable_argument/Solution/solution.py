2588a3be06fe12bd67f013940d5c962387796457
jakaiimeee <jakaiimee12@gmail.com>
2020-05-29 11:34:13 -0400

    solved Va

--- introduction_to_python/functions/2_variable_argument/Solution/solution.py
+++ introduction_to_python/functions/2_variable_argument/Solution/solution.py
@@ -1,10 +1,9 @@
-# Code your solution here
 def length(*data):
     val=''
     for i in data:
         val=val+i
     return val 
-result=length('Welcome ','To ','Byte ','Academy ')
+result=length('Welcome ','To ','Byte ','Academy ', 'Jakai')
 print(result)
 
 
