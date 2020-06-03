2b873cf94662c3e269da48a57e3ff41818b90c47
jakaiimeee <jakaiimee12@gmail.com>
2020-06-03 00:58:14 -0400

    solved insertt

--- data_structures/dictionaries_and_arrays/1.create_update_dict/Solutions/solution.py
+++ data_structures/dictionaries_and_arrays/1.create_update_dict/Solutions/solution.py
@@ -6,3 +6,10 @@ Dict[0] = 'Bob'
 Dict[1] = 'Stop'
 Dict[2] = 1,2,3
 print(Dict) 
+
+from array import array
+arr = array('i', [1, 2, 3, 4, 5])
+ins_pos = int(input("enter the position where value to be inserted"))
+ins_val = int(input("enter the value to be insterted"))
+arr.insert(ins_pos,ins_val)
+print(arr)
