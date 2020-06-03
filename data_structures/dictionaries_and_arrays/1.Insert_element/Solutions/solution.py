394def29fc35f81cb6fca81bc26396ed3ed87101
jakaiimeee <jakaiimee12@gmail.com>
2020-06-03 01:05:40 -0400

    solvedinsert really

--- data_structures/dictionaries_and_arrays/1.Insert_element/Solutions/solution.py
+++ data_structures/dictionaries_and_arrays/1.Insert_element/Solutions/solution.py
@@ -1,5 +1,5 @@
 from array import array
-arr = array('i', [1, 2, 3, 4, 5])
+arr = array('i', [1, 2, 9, 4, 8])
 ins_pos = int(input("enter the position where value to be inserted"))
 ins_val = int(input("enter the value to be insterted"))
 arr.insert(ins_pos,ins_val)
