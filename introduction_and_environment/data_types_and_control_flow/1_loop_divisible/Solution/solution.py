2a73a7bf61166e92d04e42059321cc9c5137b527
jakaiimeee <jakaiimee12@gmail.com>
2020-07-01 09:46:03 -0400

    first push

--- introduction_and_environment/data_types_and_control_flow/1_loop_divisible/Solution/solution.py
+++ introduction_and_environment/data_types_and_control_flow/1_loop_divisible/Solution/solution.py
@@ -1,5 +1,5 @@
 lower=int(input("Enter lower range limit:"))
 upper=int(input("Enter upper range limit:"))
 for i in range(lower,upper+1):
-    if(i%5==0 or i%7==0):
+    if(i%5==0 or i%9==0):
         print(i)
